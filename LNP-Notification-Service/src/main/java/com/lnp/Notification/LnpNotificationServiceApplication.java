295a34f46ec8b35260b51612689ce95664cf7a33
Dev1 <shekhardpd@gmail.com>
2024-06-22 09:28:02 +0530

    First Commit

---
+++ LNP-Notification-Service/src/main/java/com/lnp/Notification/LnpNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lnp.Notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnpNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LnpNotificationServiceApplication.class, args);
+	}
+
+}
